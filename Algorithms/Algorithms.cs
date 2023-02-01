8c32373ee4167ecc8999a6d5ad48ac4156fb9740
RadhikaPadarthi <radhika.padarthi88@gmail.com>
2023-01-31 18:12:09 -0600

    Update Algorithms.cs

--- Algorithms/Algorithms.cs
+++ Algorithms/Algorithms.cs
@@ -8,4 +8,23 @@ namespace DeveloperSample.Algorithms
 
         public static string FormatSeparators(params string[] items) => throw new NotImplementedException();
     }
+    public static int GetFactorial(int n)
+    {
+        double result =1;
+        try
+        {
+            
+            while(n !=1)
+            {
+                result= result*n;
+                n= n-1;
+            }
+        }
+        catch(Exception ex)
+        {
+            throw  new NotImplementedException();
+        }
+        return result;
+    }
+    
 }
